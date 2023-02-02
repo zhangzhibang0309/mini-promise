d9a03e4941e165a8167bf08eb8a3457a626033d1
zhangzhibang <295854713@qq.com>
2023-02-03 02:26:48 +0800

    feat: 解决链式调用（存在返回值判断问题）

--- promise.js
+++ promise.js
@@ -2,6 +2,8 @@ const PENDING = "PENDING";
 const FULFILLED = "FULFILLED";
 const REJECTED = "REJECTED";
 
+function resolvePromise(promise2, x, resolve, reject) {}
+
 export default class Promise {
   constructor(exec) {
     this.status = PENDING;
@@ -36,23 +38,60 @@ export default class Promise {
   }
 
   then(onFulfilled, onRejected) {
-    switch (this.status) {
-      case FULFILLED:
-        onFulfilled(this.res);
-        break;
-      case REJECTED:
-        onRejected(this.err);
-        break;
-      case PENDING:
+    let promise2 = new Promise((resolve, reject) => {
+      if (this.status === FULFILLED) {
+        process.nextTick(() => {
+        try {
+          let x = onFulfilled(this.res);
+          // 假设第二层promise成功 那么就调用resolve
+          // 但这里其实是有很多情况，所以封装成一个方法去判断
+          // 用x的值来判断promise2是成功还是失败
+
+          // 这里有个好奇怪的问题，你只要log promise2他就不会执行，十分诡异，promise2在这个时候还不存在，那也应该是undefined？？？
+          // 但是promise2直接用的话会报错，所以要搞个异步任务，让他的执行滞后
+          console.log(promise2, "====");
+
+          resolvePromise(promise2, x, resolve, reject);
+        } catch (err) {
+          reject(err);
+        }
+        });
+      }
+      if (this.status === REJECTED) {
+        process.nextTick(() => {
+          try {
+            let x = onRejected(this.err);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (err) {
+            reject(err);
+          }
+        });
+      }
+      if (this.status === PENDING) {
         // 注册、订阅
         this.onReslovedCallbacks.push((res) => {
-          // TODO 可扩展
-          onFulfilled(res);
+          // TODO 可扩展 这就是切面编程
+          process.nextTick(() => {
+            try {
+              let x = onFulfilled(res);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (err) {
+              reject(err);
+            }
+          });
         });
         this.onRejectedCallbacks.push((err) => {
-          onRejected(err);
+          process.nextTick(() => {
+            try {
+              let x = onRejected(err);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (err) {
+              console.log(err);
+            }
+          });
         });
-        break;
-    }
+      }
+    });
+    return promise2;
   }
 }
